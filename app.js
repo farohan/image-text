f074811923d17883e53e3cd8320dbccb71399ea7
farohan <68710283+farohan@users.noreply.github.com>
2023-05-03 18:32:35 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,11 @@
 const form = document.querySelector('#form');
 const input = document.querySelector('#input');
 
-const canvas = document.querySelector('#canvas');
-const ctx = canvas.getContext('2d');
+const imgBox = document.querySelector('#image-box');
+
+const outputBox = document.querySelector('#result-box');
+const output = document.querySelector('#results');
+const copyBtn = document.querySelector('#copy-btn');
 
 form.addEventListener('submit', formHandler);
 
@@ -26,15 +29,30 @@ function postData() {
         processData: false,
         contentType: false, 
         success: function (result) {
+            outputBox.style.visibility = 'visible';
+            copyBtn.style.visibility = 'visible';
+            output.innerHTML = '';
             console.log(result);
-            const img = new Image();
-            img.src = image;
-            img.onload = () => {
-                ctx.drawImage(img, 0, 0);
-            };
+            for (let i = 0; i <= result.length; i++) {
+                output.innerHTML += `${result[i].text} `;
+            }
         },
         error: function ajaxError(jqXHR) {
             alert(jqXHR.responseText);
         }
     });
+}
+
+input.addEventListener('change', function(e) {
+    imgBox.innerHTML = '';
+
+    let newImg = new Image(800, 600);
+    newImg.src = e.target.files[0];
+    newImg.src = URL.createObjectURL(e.target.files[0]);
+    imgBox.appendChild(newImg);
+});
+
+function copyText() {
+    navigator.clipboard.writeText(output.innerHTML);
+    alert(`You just copied: ${output.innerHTML}`);
 }
